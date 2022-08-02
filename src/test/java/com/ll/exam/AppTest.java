f0e18414bd164dcf21024bc275f06368a0a02f0d
Aruming <0430kar@naver.com>
2022-08-02 13:27:11 +0900

    AppTest 수정

--- src/test/java/com/ll/exam/AppTest.java
+++ src/test/java/com/ll/exam/AppTest.java
@@ -32,14 +32,6 @@ public class AppTest {
     }
 
     @Test
-    public void ioc__Controller들을_스캔하여_수집() {
-        List<String> names = Container.getControllerNames();
-
-        assertThat(names).contains("home");
-        assertThat(names).contains("article");
-    }
-
-    @Test
     public void ioc__homeController() {
         HomeController homeController = Container.getHomeController();
 
@@ -47,11 +39,18 @@ public class AppTest {
     }
 
     @Test
-    public void ioc__homeController__싱글톤() {
+    public void ioc__homeController__singleton() {
         HomeController homeController1 = Container.getHomeController();
         HomeController homeController2 = Container.getHomeController();
 
         assertThat(homeController2).isEqualTo(homeController1);
     }
 
+    @Test
+    public void ioc__Controller들을_스캔하여_수집() {
+        List<String> names = Container.getControllerNames();
+
+        assertThat(names).contains("home");
+        assertThat(names).contains("article");
+    }
 }
