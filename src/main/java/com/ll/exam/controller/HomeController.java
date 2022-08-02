8d21ab39dee98e05a5f36a5b77da948d4a14ca0e
Aruming <0430kar@naver.com>
2022-08-02 11:09:09 +0900

    컨트롤러 스캔 테스트케이스 작성

---
+++ src/main/java/com/ll/exam/controller/HomeController.java
@@ -0,0 +1,7 @@
+package com.ll.exam.controller;
+
+import com.ll.exam.annotation.Controller;
+
+@Controller
+public class HomeController {
+}
--- src/test/java/com/ll/exam/AppTest.java
+++ src/test/java/com/ll/exam/AppTest.java
@@ -2,6 +2,9 @@ package com.ll.exam;
 
 import com.ll.exam.controller.ArticleController;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class AppTest {
@@ -25,5 +28,13 @@ public class AppTest {
         ArticleController articleController2 = Container.getArticleController();
 
         assertThat(articleController2).isEqualTo(articleController1);
+    }
+
+    @Test
+    public void ioc__Controller들을_스캔하여_수집() {
+        List<String> names = Container.getControllerNames();
+
+        assertThat(names).contains("home");
+        assertThat(names).contains("article");
     }
 }
